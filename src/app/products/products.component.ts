6ddac3e1358f7916555b91a9fe267290b7b57136
Caitlin <caitlin@griffus.com>
2019-08-28 15:35:39 -0400

    working add form

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-products",
@@ -8,12 +9,39 @@ import { CartService } from "../cart.service";
 })
 export class ProductsComponent implements OnInit {
   cart: any[];
+  showAddForm: boolean = false;
+  itemToEdit: object;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit() {
-    this.cartService
-      .getAllItems()
-      .subscribe(response => (this.cart = response));
+    this.cartService.getAllItems().subscribe(response => {
+      this.cart = response;
+    });
+  }
+
+  toggleAddForm(): void {
+    this.showAddForm = !this.showAddForm;
+  }
+
+  setItemToEdit(item: object): void {
+    this.itemToEdit = item;
+  }
+
+  addNewItem(form: NgForm): void {
+    this.cartService.postItems(form.value).subscribe(response => {
+      this.cart = response;
+    });
+  }
+
+  deleteItem(id: number): void {
+    console.log(id);
+    this.cartService.deleteItems(id).subscribe(response => {
+      this.cart = response;
+    });
+  }
+
+  updateItem(form: NgForm): void {
+    console.log(form.value);
   }
 }
